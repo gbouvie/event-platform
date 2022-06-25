818d1ddcee9c15677fefd6ecd4464ffe00aa40cf
Giovani Bouvie <gbouvie@gmail.com>
2022-06-25 16:26:33 -0300

    home page creation with form

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -12,7 +12,10 @@ const defaultOptions: DefaultOptions = {
 };
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o2kck209f401xm4l7bb7wh/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+	'Authorizarion': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache(),
 	defaultOptions: defaultOptions,
 });
